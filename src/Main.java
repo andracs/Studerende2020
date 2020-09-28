9ce9b5ad4052ce7eeff7b7a4cccba956b9f8b6b4
András Ács <acsandras@gmail.com>
2020-09-28 10:19:59 +0200

    Løsning på switch er lavet

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Developed by András Ács (acsandras@gmail.com)
  * Zealand / www.zealand.dk
@@ -10,17 +12,17 @@ public class Main {
     public static void main(String[] args) {
 
         Studerende st1 = new Studerende();
-        st1.navn ="Lasse";
+        st1.navn = "Lasse";
         st1.alder = 22;
         st1.by = "Skuderløse";
 
         Studerende st2 = new Studerende();
-        st2.navn ="Asta";
+        st2.navn = "Asta";
         st2.alder = 22;
         st2.by = "Haslev";
 
         Studerende st3 = new Studerende();
-        st3.navn ="Delal";
+        st3.navn = "Delal";
         st3.alder = 20;
         st3.by = "Albertslund";
 
@@ -29,6 +31,26 @@ public class Main {
         // switch (studerende) {
         //    "Lasse" sout studerende.alder
         // }
+
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Hvem vil du slå op i databasen? ");
+            String nameToFind = scanner.next();
+            switch (nameToFind) {
+                case "Lasse":
+                    System.out.println(st1.alder);
+                    break;
+                case "Asta":
+                    System.out.println(st2.alder);
+                    break;
+                case "Delal":
+                    System.out.println(st3.alder);
+                    break;
+                default:
+                    System.out.println(nameToFind + " er ikke med i min studiegruppe.");
+            }
+        }
+
 
     }
 
